587e789cf38a31fb22f167672c7f7effa8a5dba0
Mish Ushakov <mishushakov@users.noreply.github.com>
2024-08-30 22:02:48 +0200

    updated sample

--- app/sandbox.ts
+++ app/sandbox.ts
@@ -9,14 +9,22 @@ export async function createSandbox(code: string) {
   // Create sandbox
   const sandbox = await Sandbox.create(template);
 
-  // Copy code to the sandbox
+  // Copy the code to the sandbox
   await sandbox.filesystem.write(filename, code);
 
+  // Start the dev server
+  await sandbox.process.start("cd /home/user && reflex run");
+
+  // Get the server URL
   const url = await sandbox.getHostname(3000);
 
-  // run the healthcheck - will start and wait for the server to be ready
+  // Run the healthcheck - will start and wait for the dev server to be ready
   await sandbox.process.startAndWait("python3 /home/user/healthcheck.py " + url);
 
+  // Run healthcheck again to make sure the server is fully reloaded
+  // await sandbox.process.startAndWait("python3 /home/user/healthcheck.py " + url);
+
+  // Return the URL
   return {
     url: `https://${url}`,
   };
